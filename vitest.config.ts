d29a4a4b8ec6aee2439ae7437c8e591f99df156c
KumJungMin <rmawjdals@naver.com>
2025-01-06 21:19:54 +0900

    chore: setting coverage compare workflow

--- vitest.config.ts
+++ vitest.config.ts
@@ -8,6 +8,7 @@ export default defineConfig({
       provider: 'v8',
       reporter: ['text', 'json-summary', 'lcov'], 
       reportsDirectory: './coverage', 
+      include: ['project/**/*.{ts,tsx,js,jsx}'],
     },
   },
 })
