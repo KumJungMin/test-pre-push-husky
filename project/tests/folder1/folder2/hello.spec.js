5341bffbbf65d3ddd1e3b9394c97d7948b616e38
KumJungMin <rmawjdals@naver.com>
2025-01-21 19:41:00 +0900

    222

--- project/tests/folder1/folder2/hello.spec.js
+++ project/tests/folder1/folder2/hello.spec.js
@@ -1,3 +1,4 @@
+import { describe } from 'node:test';
 import { sum, multiply } from '../../../folder1/folder2/hello';
 
 describe('sum', () => {
@@ -5,3 +6,9 @@ describe('sum', () => {
     expect(sum(1, 2)).toBe(3);
   });
 });
+
+describe ('multiply', () => {
+  it('multiplies 2 * 3 to equal 6', () => {
+    expect(multiply(2, 3)).toBe(6);
+  });
+})
